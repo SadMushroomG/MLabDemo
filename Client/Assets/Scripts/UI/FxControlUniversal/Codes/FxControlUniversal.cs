75aa18fb8c10102e06402ae30292f042e08bb92f
maoshu <maoshumo@lilith.com>
2025-04-10 01:19:36 +0800

    毛叔 - FXControl上传

---
+++ Client/Assets/Scripts/UI/FxControlUniversal/Codes/FxControlUniversal.cs
@@ -0,0 +1,426 @@
+﻿//ver3.5.4 iGame 组件复制适配 https://lilithgames.feishu.cn/wiki/wikcnDvbLbdSIuTZ0ZDy5K81MRe
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+//[RequireComponent(typeof(RawImage))]
+[ExecuteAlways]
+public class FxControlUniversal : MonoBehaviour
+{
+    public bool controlOtherObject = false;
+    public GameObject targetObject;
+    // isDifferentMat
+    public Material matOrigin;                  // public:m_matOrigin can be inherited
+    public Material matCurrent;                 // public:m_matCurrent can be inherited
+    private Material m_matDefault = null;
+    //public RawImage rawImage = null;            // public:editor can get the value 
+    //public Image image = null;                  // public:editor can get the value 
+    public Graphic image = null;                // public:editor can get the value 
+    public Renderer render = null;
+    public TMP_Text tmPro = null;
+    
+    public bool isDifferent = false;
+
+    public bool isControl = true;
+    private bool m_preIsDifferent = false;
+    private bool m_preIsPlaying = false;
+    private bool[] m_isHadProperty = new bool[9];
+    private bool[] m_keywordState = new bool[2];
+
+    // float
+    public string _stringF1 = null;
+    public string _stringF2 = null;
+    public string _stringF3 = null;
+    public string _stringF4 = null;
+    public string _stringF5 = null;
+    public float _float1 = 0;
+    public float _float2 = 0;
+    public float _float3 = 0;
+    public float _float4 = 0;
+    public float _float5 = 0;
+    
+    public string _stringKW1 = null;
+    public string _stringKW2 = null;
+    public float _keywordVal1 = 0;
+    public float _keywordVal2 = 0;
+
+    // vector
+    public string _stringV1 = null;
+    public string _stringV2 = null;
+    public Vector4 _vector1 = new Vector4(1, 1, 0, 0);
+    public Vector4 _vector2 = new Vector4(1, 1, 0, 0);
+    public Vector4 _vectoring = new Vector4(1, 1, 0, 0);
+
+    // color
+    public string _stringC1 = null;
+    public string _stringC2 = null;
+    [ColorUsage(true, true)]
+    public Color _color1 = Color.white;
+    [ColorUsage(true, true)]
+    public Color _color2 = Color.white;
+
+    private GameObject targetGo;
+
+    /// <summary>
+    /// 首次应用材质参数
+    /// </summary>
+    void InitOnceMatParam()
+    {
+        targetGo = controlOtherObject ? targetObject : gameObject;
+        if (targetGo == null)
+        {
+            return;
+        }
+
+        if (image != null || render != null || tmPro != null)
+        {
+            return;
+        }
+        image = targetGo.GetComponent<Graphic>();
+        tmPro = targetGo.GetComponent<TMP_Text>();
+         if (image == null&tmPro == null)
+        {
+            render = targetGo.GetComponent<Renderer>();
+        }
+    
+        if (image != null & tmPro == null)
+        {
+            if (image.material && image.material.name.StartsWith("[Copy]"))
+            {
+                return;
+            }
+            matOrigin = matCurrent = image.material;
+            m_matDefault = image.defaultMaterial;
+            
+            Debug.Assert(m_matDefault != null, "Ojbect: " + this.name + " m_matDefault is null");
+            Debug.Assert(matOrigin != null, "Ojbect: " + this.name + " .m_matOrigin is null");
+            Debug.Assert(matCurrent != m_matDefault, "Ojbect: " + this.name + " Has Material Which Cant Not Be Default UI");
+            return;
+        }
+        else if (render != null)
+        {
+            if (render.sharedMaterial && render.sharedMaterial.name.StartsWith("[Copy]"))
+            {
+                return;
+            }
+            matOrigin = matCurrent = render.sharedMaterial;
+            m_matDefault = render.sharedMaterial;
+
+            // Debug.Log(render.sharedMaterial.name+"11");
+            return;
+
+        }
+        else if(tmPro != null)
+        {
+            if (tmPro.fontSharedMaterial && tmPro.fontSharedMaterial.name.StartsWith("[Copy]"))
+            {
+                return;
+            }
+            matOrigin = matCurrent = tmPro.fontSharedMaterial;
+            m_matDefault = tmPro.fontSharedMaterial;
+            return;
+        }
+
+        if(!controlOtherObject)
+            Debug.LogError( "Ojbect: " + this.name + ", Not Found Component:RawImage Or Image Or Renderer");
+    }
+
+    /// <summary>
+    /// 是否应用差异材质球
+    /// </summary>
+    void ApplyDifferentMaterial()
+    {
+        if (matOrigin == null)
+        {
+            return;
+        }
+        
+        if (m_preIsDifferent == isDifferent && matCurrent)
+        {
+            return;
+        }
+        m_preIsDifferent = isDifferent;
+        if (isDifferent && matOrigin && !matOrigin.name.StartsWith("[Copy]"))
+        {
+            matCurrent = new Material(matOrigin);
+            matCurrent.name = "[Copy]" + matOrigin.name;
+        }
+        else
+        {
+            if (image != null && tmPro == null && matCurrent != m_matDefault && matCurrent != matOrigin)
+            {
+                DestroyImmediate(matCurrent);
+            }
+            else if (render != null && matCurrent != m_matDefault)
+            {
+                DestroyImmediate(matCurrent);
+            }
+            else if(tmPro != null)
+            {
+                DestroyImmediate(matCurrent);
+            }
+           matCurrent = matOrigin;
+
+
+        }
+        if (image != null & tmPro == null)
+        {
+            image.material = matCurrent;
+        }
+       else if (render != null)
+        {
+            render.sharedMaterial = matCurrent;
+ 
+        }
+        else if(tmPro != null)
+        {
+            tmPro.fontSharedMaterial = matCurrent;
+        }
+    }
+
+    /// <summary>
+    /// 检查参数是否存在&&是否发生变化
+    /// </summary>
+    void CheckAllProperty()
+    {
+        int i = 0;
+        CheckEachString(_stringF1, i++);
+        CheckEachString(_stringF2, i++);
+        CheckEachString(_stringF3, i++);
+        CheckEachString(_stringF4, i++);
+        CheckEachString(_stringF5, i++);
+        CheckEachString(_stringV1, i++);
+        CheckEachString(_stringV2, i++);
+        CheckEachString(_stringC1, i++);
+        CheckEachString(_stringC2, i);
+    }
+
+    /// <summary>
+    /// 检查string值是否存在
+    /// </summary>
+    /// <param name="_string"></param>
+    /// <param name="_index"></param>
+    void CheckEachString(string _string, int _index)
+    {
+        m_isHadProperty[_index] = _string != "" && matCurrent != null && matCurrent.HasProperty(_string) ? true : false;
+    }
+
+    /// <summary>
+    /// 应用材质球参数
+    /// </summary>
+    void ApplyMaterialParam()
+    {
+        //这里是1.2.1加进去的代码，1.3.1生效 by 毛叔石琦
+        if (targetGo == null || (controlOtherObject && (targetGo != targetObject)))
+        {
+            targetGo = controlOtherObject ? targetObject : gameObject;
+            render = null;
+            image = null;
+            tmPro = null;
+            m_preIsDifferent = false;
+            if (Application.isPlaying)
+            {
+                if (isDifferent & matCurrent != m_matDefault)
+                {
+                    DestroyImmediate(matCurrent);
+                }
+                matOrigin = null;
+                m_matDefault = null;
+            }
+            if (targetGo == null)
+                return;
+            InitMatParam();
+        }
+
+        if (matCurrent != null && matOrigin != null && matCurrent == matOrigin)//&& image.material != matOrigin)
+        {   // 是否更换材质，重新赋默认值
+            if (image != null & tmPro == null)
+            {
+                if (image.material != matOrigin)
+                {
+                    matOrigin = matCurrent = image.material;
+                }
+
+            }
+           else if (render != null)
+            {
+                if (render.sharedMaterial != matOrigin)
+                {
+                    matOrigin = matCurrent = render.sharedMaterial;
+                }
+
+            }
+            else if(tmPro != null)
+            {
+                   if (tmPro.fontSharedMaterial != matOrigin)
+                {
+                    matOrigin = matCurrent = tmPro.fontSharedMaterial;
+                }
+            }
+
+
+        }
+        int i = 0;
+        // float
+        IsNeedSetFloat(_stringF1, _float1, i++);
+        IsNeedSetFloat(_stringF2, _float2, i++);
+        IsNeedSetFloat(_stringF3, _float3, i++);
+        IsNeedSetFloat(_stringF4, _float4, i++);
+        IsNeedSetFloat(_stringF5, _float5, i++);
+
+        // vector
+        IsNeedSetVector(_stringV1, _vector1, i++);
+        IsNeedSetVector(_stringV2, _vector2, i++);
+
+        // color
+        IsNeedSetColor(_stringC1, _color1, i++);
+        IsNeedSetColor(_stringC2, _color2, i);
+
+        // keyword
+        SetKeyword(_stringKW1, _keywordVal1);
+        SetKeyword(_stringKW2, _keywordVal2);
+    }
+
+    private void OnEnable()
+    {
+        InitMatParam();
+
+        if (isControl)
+        {
+            ApplyMaterialParam();
+        }
+    }
+    private void OnDisable()
+    {
+        //image = null;
+        //tmPro = null;
+        //render = null;
+        m_preIsDifferent = false;
+    }
+    void OnValidate()
+    {
+        InitMatParam();
+    }
+
+    public void InitMatParam()
+    {
+        InitOnceMatParam();
+        ApplyDifferentMaterial();
+
+        CheckAllProperty();
+    }
+
+    void LateUpdate()
+    {
+        var curIsPlaying = Application.isPlaying;
+        if (m_preIsPlaying && !curIsPlaying)
+        {
+            InitMatParam();
+        }
+        m_preIsPlaying = curIsPlaying;
+
+        if (isControl)
+        {
+            ApplyMaterialParam();
+        }
+    }
+
+    /// <summary>
+    /// 设置float
+    /// </summary>
+    /// <param name="_string"></param>
+    /// <param name="_float"></param>
+    /// <param name="_index"></param>
+    private void IsNeedSetFloat(string _string, float _float, int _index)
+    {
+        if (m_isHadProperty[_index])   //_string != "" && matCurrent.HasProperty(_string)
+        {
+            float _value = matCurrent.GetFloat(_string);
+            if (_value != _float)
+            {
+                matCurrent.SetFloat(_string, _float);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置vector
+    /// </summary>
+    /// <param name="_string"></param>
+    /// <param name="_vector"></param>
+    /// <param name="_index"></param>
+    private void IsNeedSetVector(string _string, Vector4 _vector, int _index)
+    {
+        if (m_isHadProperty[_index])
+        {
+            Vector4 _value = matCurrent.GetVector(_string);
+            if (_value != _vector)
+            {
+                matCurrent.SetVector(_string, _vector);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 设置color
+    /// </summary>
+    /// <param name="_string"></param>
+    /// <param name="_color"></param>
+    /// <param name="_index"></param>
+    private void IsNeedSetColor(string _string, Color _color, int _index)
+    {
+        if (m_isHadProperty[_index])
+        {
+            Color _value = matCurrent.GetColor(_string);
+            if (_value != _color)
+            {
+                matCurrent.SetColor(_string, _color);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 设置Keyword
+    /// </summary>
+    private void SetKeyword(string _string, float _value)
+    {
+        if (_string == "")
+            return;
+
+        if (_value == 1.0f)
+        {
+            if (matCurrent.IsKeywordEnabled(_string))
+                return;
+            matCurrent.EnableKeyword(_string);
+        }
+        else
+        {
+            if (!matCurrent.IsKeywordEnabled(_string))
+                return;
+            matCurrent.DisableKeyword(_string);
+        }
+    }
+
+    public void RefreshMaterial()
+    {
+        matCurrent = matOrigin =  image.material;
+        m_preIsDifferent = false;
+        ApplyDifferentMaterial();
+        Debug.Log("[TATest]Refresh Material");
+    }
+
+     void OnDestroy()
+    {
+        if (Application.isPlaying)
+        {
+            if (isDifferent & matCurrent != m_matDefault)
+            {
+                DestroyImmediate(matCurrent);
+            }
+            matOrigin = null;
+            m_matDefault = null;
+        }
+    }
+}
+
---
+++ Client/Assets/Scripts/UI/FxControlUniversal/Editor/FxControlUniversalEditor.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(FxControlUniversal))]
+public class FxControlUniversalEditor : UnityEditor.Editor
+{
+    private SerializedProperty _stringF1, _stringF2, _stringF3, _stringF4, _stringF5,
+        _float1, _float2, _float3, _float4, _float5,
+        _stringV1, _stringV2, _vector1, _vector2,
+        _stringC1, _stringC2, _color1, _color2,
+        _isDifferent, _image ,_isControl;
+    private SerializedProperty _stringKW1, _stringKW2, _keywordVal1, _keywordVal2;
+    private SerializedProperty _controlOtherObject, _targetObject, _matOrigin;
+    private bool[] isBool = new bool[11];
+    private bool isFloat = true, isVector = true, isColor = true, isKeyword = true;
+    void OnEnable()
+    {
+        _controlOtherObject = serializedObject.FindProperty("controlOtherObject");
+        _targetObject = serializedObject.FindProperty("targetObject");
+
+        // float
+        _stringF1 = serializedObject.FindProperty("_stringF1");
+        _float1 = serializedObject.FindProperty("_float1");
+        _stringF2 = serializedObject.FindProperty("_stringF2");
+        _float2 = serializedObject.FindProperty("_float2");
+        _stringF3 = serializedObject.FindProperty("_stringF3");
+        _float3 = serializedObject.FindProperty("_float3"); ;
+        _stringF4 = serializedObject.FindProperty("_stringF4");
+        _float4 = serializedObject.FindProperty("_float4");
+        _stringF5 = serializedObject.FindProperty("_stringF5");
+        _float5 = serializedObject.FindProperty("_float5");
+
+
+        // vector
+        _stringV1 = serializedObject.FindProperty("_stringV1");
+        _vector1 = serializedObject.FindProperty("_vector1");
+        _stringV2 = serializedObject.FindProperty("_stringV2");
+        _vector2 = serializedObject.FindProperty("_vector2");
+
+        // color
+        _stringC1 = serializedObject.FindProperty("_stringC1");
+        _stringC2 = serializedObject.FindProperty("_stringC2");
+        _color1 = serializedObject.FindProperty("_color1");
+        _color2 = serializedObject.FindProperty("_color2");        
+        
+        
+        // color
+        _stringKW1 = serializedObject.FindProperty("_stringKW1");
+        _stringKW2 = serializedObject.FindProperty("_stringKW2");
+        _keywordVal1 = serializedObject.FindProperty("_keywordVal1");
+        _keywordVal2 = serializedObject.FindProperty("_keywordVal2");
+
+        // 是否设置material相关判定参数
+        _isDifferent = serializedObject.FindProperty("isDifferent");
+        _image = serializedObject.FindProperty("image");
+        _matOrigin = serializedObject.FindProperty("matOrigin");
+
+        // 是否实时控制材质球参数
+        _isControl = serializedObject.FindProperty("isControl");
+       
+    }
+
+    public override void OnInspectorGUI()
+    {
+        FxControlUniversal script = target as FxControlUniversal;
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUI.indentLevel++;
+        EditorGUILayout.LabelField("-Shader参数控制脚本：");
+        EditorGUI.indentLevel++;
+        EditorGUILayout.LabelField("-string输入变量名(_XxxYyy)，对变量值(Float,Vector,Color)K帧");
+        EditorGUI.indentLevel--;
+        EditorGUI.indentLevel--;
+
+        // Material材质的提示文字显示
+
+        if (_image.objectReferenceValue != null)
+        {
+            Graphic graphic = _image.objectReferenceValue as Graphic;
+            if (graphic.material == graphic.defaultMaterial)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("!!!! 需要附加Material材质 !!!!");
+                EditorGUILayout.Space();
+            }
+
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(_controlOtherObject, new GUIContent("控制其他物体材质"));
+        if(_controlOtherObject.boolValue)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(_targetObject, new GUIContent("需要被控制材质的gameobject"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                script.InitMatParam();
+            }
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.Space();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(_isDifferent, new GUIContent("独立材质球(勾选后生成可单独控制的材质球_默认不勾选)"));
+        if (_isDifferent.boolValue)
+        {
+            EditorGUI.indentLevel++;
+            if (GUILayout.Button("刷新材质球"))
+            {
+                script.RefreshMaterial();
+            }
+            GUI.enabled = false;
+            EditorGUILayout.PropertyField(_matOrigin, new GUIContent("原先使用材质球索引-只读"));
+            GUI.enabled = true;
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.PropertyField(_isControl, new GUIContent("实时控制材质球参数(默认勾选_仅在调试时使用)"));
+        EditorGUILayout.Space();
+
+        //float
+        isFloat = EditorGUILayout.Foldout(isFloat, "Float变量", true);
+        if (isFloat)
+        {
+            EditorGUI.indentLevel++;
+            isBool[0] = ParamKeyFrame(isBool[0], _stringF1, _float1, "KeyFrame_Float");
+            isBool[1] = ParamKeyFrame(isBool[1], _stringF2, _float2, "KeyFrame_Float");
+            isBool[2] = ParamKeyFrame(isBool[2], _stringF3, _float3, "KeyFrame_Float");
+            isBool[3] = ParamKeyFrame(isBool[3], _stringF4, _float4, "KeyFrame_Float");
+            isBool[4] = ParamKeyFrame(isBool[4], _stringF5, _float5, "KeyFrame_Float");
+            EditorGUI.indentLevel--;
+        }
+        //vector
+        isVector = EditorGUILayout.Foldout(isVector, "Vector变量", true);
+        if (isVector)
+        {
+            EditorGUI.indentLevel++;
+            isBool[5] = ParamKeyFrame(isBool[5], _stringV1, _vector1, "KeyFrame_Vector");
+            isBool[6] = ParamKeyFrame(isBool[6], _stringV2, _vector2, "KeyFrame_Vector");
+            EditorGUI.indentLevel--;
+        }
+        //color
+        isColor = EditorGUILayout.Foldout(isColor, "Color变量", true);
+        if (isColor)
+        {
+            EditorGUI.indentLevel++;
+            isBool[7] = ParamKeyFrame(isBool[7], _stringC1, _color1, "KeyFrame_Color");
+            isBool[8] = ParamKeyFrame(isBool[8], _stringC2, _color2, "KeyFrame_Color");
+            EditorGUI.indentLevel--;
+        }
+
+        //color
+        isKeyword = EditorGUILayout.Foldout(isKeyword, "Keyword变量", true);
+        if (isKeyword)
+        {
+            EditorGUI.indentLevel++;
+            isBool[9] = ParamKeyFrame(isBool[9], _stringKW1, _keywordVal1, "Keyword_1");
+            isBool[10] = ParamKeyFrame(isBool[10], _stringKW2, _keywordVal2, "Keyword_2");
+            EditorGUI.indentLevel--;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    /// <summary>
+    /// 变量GUI设置传值
+    /// </summary>
+    /// <param name="isBool"></param>
+    /// <param name="_string"></param>
+    /// <param name="_type"></param>
+    /// <param name="_label"></param>
+    /// <returns></returns>
+    private bool ParamKeyFrame(bool isBool, SerializedProperty _string, SerializedProperty _type, string _label)
+    {
+        // string不为空时，展开具体变量
+        if (_string.stringValue != "")
+        {
+            isBool = true;
+        }
+        // GUI
+        isBool = EditorGUILayout.Toggle(_label, isBool);
+        if (isBool)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(_string, new GUIContent(_string.name.ToString()));
+            EditorGUILayout.PropertyField(_type, new GUIContent(_type.name.ToString()));
+            EditorGUI.indentLevel--;
+        }
+        return isBool;
+    }
+}
